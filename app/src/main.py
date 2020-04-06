7ac9212e7d1a923b36aca52af66e3cb8bb8aaa66
jwross24 <jwross24@users.noreply.github.com>
2020-04-06 18:45:36 -0400

    experiment with Request Body

--- app/src/main.py
+++ app/src/main.py
@@ -1,6 +1,14 @@
 from enum import Enum
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str = None
+    price: float
+    tax: float = None
 
 
 class ModelName(str, Enum):
@@ -13,6 +21,23 @@ app = FastAPI()
 
 fake_items_db = [{"item_name": "Foo"}, {
     "item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: str = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
 
 
 @app.get("/users/{user_id}/items/{item_id}")
