03ae6d6125c6605479e6cad558a8346ec4b5816e
jwross24 <jwross24@users.noreply.github.com>
2020-04-06 18:26:56 -0400

    experiment with query parameters

--- app/src/main.py
+++ app/src/main.py
@@ -11,6 +11,23 @@ class ModelName(str, Enum):
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {
+    "item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
 
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
@@ -37,8 +54,9 @@ async def read_user(user_id: str):
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+async def read_user_item(item_id: str, needy: str, skip: int = 0, limit: int = None):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
 
 
 @app.get("/")
